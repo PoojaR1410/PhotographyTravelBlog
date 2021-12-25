158054cfc6b53df48761a2f1ac9a3a751945754a
PoojaR1410 <poojaraut1410@gmail.com>
2021-12-25 11:42:36 +0530

    TravelBlog

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  
+  }
+
+
+
+}
+
